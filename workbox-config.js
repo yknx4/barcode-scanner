67eda453beb02fcc8ab7702d03556642d54d4038
Jade Ornelas <jade@ornelas.io>
2024-10-31 11:36:17 -0400

    feat: export with less generic names

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: 'dist',
   globPatterns: ['**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'],
-  swDest: 'dist/service-worker.js',
+  swDest: 'dist/barcode-scanner-worker.js',
   clientsClaim: true,
   skipWaiting: true
 };
